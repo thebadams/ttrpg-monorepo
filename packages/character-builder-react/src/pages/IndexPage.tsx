d7679a75a0911f692da5692d0dee661cc0bcef8f
thebadams <bpeteradams@gmail.com>
2023-08-02 12:52:51 -0400

    tRPC(all classes): Wrote an implmentation to get all classes info from the database and display in react app

--- packages/character-builder-react/src/pages/IndexPage.tsx
+++ packages/character-builder-react/src/pages/IndexPage.tsx
@@ -2,10 +2,13 @@ import { trpc } from '../utils/trpc';
 
 export default function IndexPage() {
 	const helloQuery = trpc.hello.useQuery();
+	const classesQuery = trpc.allClasses.useQuery();
+	console.log(classesQuery);
 	console.log(helloQuery.data);
 	return (
 		<div>
 			<p>{helloQuery?.data || 'No Data'}</p>
+			{classesQuery.data?.map((item) => <p>{item.name}</p>)}
 		</div>
 	);
 }
--- packages/fastify-backend/src/router.ts
+++ packages/fastify-backend/src/router.ts
@@ -1,10 +1,20 @@
 import { initTRPC } from '@trpc/server';
 import { z } from 'zod';
+import { prisma } from './prisma';
 
 export const t = initTRPC.create();
 const publicProcedure = t.procedure;
 export const appRouter = t.router({
 	hello: publicProcedure.query(() => 'Hello World'),
+	allClasses: publicProcedure.query(async () => {
+		try {
+			const classes = await prisma.class.findMany({});
+			return classes;
+		} catch (error) {
+			console.error(error);
+			return [];
+		}
+	}),
 });
 
 export type AppRouter = typeof appRouter;
