653272244e5c5a45b4f89eb4bf40d06a23c31d41
thebadams <bpeteradams@gmail.com>
2023-08-07 21:28:18 -0400

    UI(CharacterDisplay): began work on the ui and ux of the CharacterDisplay component.

--- packages/character-builder-react/src/components/CharacterDisplay.tsx
+++ packages/character-builder-react/src/components/CharacterDisplay.tsx
@@ -5,37 +5,79 @@ export default function CharacterDisplay() {
   const [currentCharacter] = useAtom(currentCharacterAtom);
   return (
     <>
-      <h2 className=" text-center text-3xl text-red-700 shadow-sm shadow-black">
-        Current Character
-      </h2>
-      <h3>CHARACTER NAME || {currentCharacter.name}</h3>
-      <h3>PLAYER NAME || {currentCharacter.player}</h3>
-      <h3>CHARACTER CLASS || {currentCharacter.class}</h3>
-      <h3>CHARACTER LEVEL || {currentCharacter.level}</h3>
-      <h3>
-        STRENGTH || {currentCharacter.abilities.strength.score} ||{' '}
-        {currentCharacter.abilities.strength.mod}
-      </h3>
-      <h3>
-        DEXTERITY || {currentCharacter.abilities.dexterity.score} ||{' '}
-        {currentCharacter.abilities.dexterity.mod}
-      </h3>
-      <h3>
-        CONSTITUTION || {currentCharacter.abilities.constitution.score} ||{' '}
-        {currentCharacter.abilities.constitution.mod}
-      </h3>
-      <h3>
-        INTELLIGENCE || {currentCharacter.abilities.intelligence.score} ||{' '}
-        {currentCharacter.abilities.intelligence.mod}
-      </h3>
-      <h3>
-        WISDOM || {currentCharacter.abilities.wisdom.score} ||{' '}
-        {currentCharacter.abilities.wisdom.mod}
-      </h3>
-      <h3>
-        CHARISMA || {currentCharacter.abilities.charisma.score} ||{' '}
-        {currentCharacter.abilities.charisma.mod}
-      </h3>
+      <div className="block rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700">
+        <h2 className="mb-2 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50">
+          Current Character
+        </h2>
+        <div className="p-6">
+          <h3 className="mb-2 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50">
+            CHARACTER NAME
+          </h3>
+          <p className="mb-4 text-base text-neutral-600 dark:text-neutral-200">
+            {currentCharacter.name}
+          </p>
+          <h3 className="mb-2 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50">
+            Player Name
+          </h3>
+          <p className="mb-4 text-base text-neutral-600 dark:text-neutral-200">
+            {currentCharacter.player}
+          </p>
+          <h3 className="mb-2 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50">
+            CLASS
+          </h3>
+          <p className="mb-4 text-base text-neutral-600 dark:text-neutral-200">
+            {currentCharacter.class}
+          </p>
+          <h3 className="mb-2 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50">
+            CHARACTER LEVEL
+          </h3>
+          <p className="mb-4 text-base text-neutral-600 dark:text-neutral-200">
+            {currentCharacter.level}
+          </p>
+          <h3 className="mb-2 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50">
+            STRENGTH
+          </h3>
+          <p className="mb-4 text-base text-neutral-600 dark:text-neutral-200">
+            {currentCharacter.abilities.strength.score} ||{' '}
+            {currentCharacter.abilities.strength.mod}
+          </p>
+          <h3 className="mb-2 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50">
+            DEXTERITY
+          </h3>
+          <p className="mb-4 text-base text-neutral-600 dark:text-neutral-200">
+            {currentCharacter.abilities.dexterity.score} ||{' '}
+            {currentCharacter.abilities.dexterity.mod}
+          </p>
+          <h3 className="mb-2 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50">
+            CONSTITUTION
+          </h3>
+          <p className="mb-4 text-base text-neutral-600 dark:text-neutral-200">
+            {currentCharacter.abilities.constitution.score} ||{' '}
+            {currentCharacter.abilities.constitution.mod}
+          </p>
+          <h3 className="mb-2 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50">
+            INTELLIGENCE
+          </h3>
+          <p className="mb-4 text-base text-neutral-600 dark:text-neutral-200">
+            {currentCharacter.abilities.intelligence.score} ||{' '}
+            {currentCharacter.abilities.intelligence.mod}
+          </p>
+          <h3 className="mb-2 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50">
+            WISDOM
+          </h3>
+          <p className="mb-4 text-base text-neutral-600 dark:text-neutral-200">
+            {currentCharacter.abilities.wisdom.score} ||{' '}
+            {currentCharacter.abilities.wisdom.mod}
+          </p>
+          <h3 className="mb-2 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50">
+            CHARISMA
+          </h3>
+          <p className="mb-4 text-base text-neutral-600 dark:text-neutral-200">
+            {currentCharacter.abilities.charisma.score} ||{' '}
+            {currentCharacter.abilities.charisma.mod}
+          </p>
+        </div>
+      </div>
     </>
   );
 }
--- packages/character-builder-react/src/components/CharacterInputForm.tsx
+++ packages/character-builder-react/src/components/CharacterInputForm.tsx
@@ -1,173 +1,172 @@
 import { useAtom } from 'jotai';
 import {
-	charismaAtom,
-	constitutionAtom,
-	dexterityAtom,
-	intelligenceAtom,
-	levelAtom,
-	nameAtom,
-	playerAtom,
-	wisdomAtom,
-	strengthAtom,
-	classAtom,
+  charismaAtom,
+  constitutionAtom,
+  dexterityAtom,
+  intelligenceAtom,
+  levelAtom,
+  nameAtom,
+  playerAtom,
+  wisdomAtom,
+  strengthAtom,
+  classAtom,
 } from '../state/currentCharacter';
 import { trpc } from '../utils/trpc';
 
 export default function CharacterInputForm() {
-	const classNameQuery = trpc.classNames.useQuery();
-	const [_name, setName] = useAtom(nameAtom);
-	const [_playerName, setPlayerName] = useAtom(playerAtom);
-	const [class_, setClass] = useAtom(classAtom);
-	const [level, setLevel] = useAtom(levelAtom);
-	const [strength, setStrength] = useAtom(strengthAtom);
-	const [dexterity, setDexterity] = useAtom(dexterityAtom);
-	const [constitution, setConstitution] = useAtom(constitutionAtom);
-	const [intelligence, setIntelligence] = useAtom(intelligenceAtom);
-	const [wisdom, setWisdom] = useAtom(wisdomAtom);
-	const [charisma, setCharisma] = useAtom(charismaAtom);
-	return (
-		<>
-			<label htmlFor="name">CHARACTER NAME INPUT || </label>
-			<input
-				type="text"
-				name="name"
-				id="name"
-				onChange={(e) => {
-					setName(e.target.value);
-				}}
-			/>
-			<br />
-			<hr />
-			<label htmlFor="playerName">PLAYER NAME INPUT || </label>
-			<input
-				type="text"
-				name="playerName"
-				id="playerName"
-				onChange={(e) => {
-					setPlayerName(e.target.value);
-				}}
-			/>
-			<br />
-			<hr />
-			<label htmlFor="class">CLASS || </label>
-			<select
-				name="class"
-				onChange={(e) => {
-					setClass(e.target.value);
-				}}
-			>
-				{classNameQuery.data?.map((c) =>
-					c.name === class_ ? (
-						<option selected value={c.name}>
-							{c.name}
-						</option>
-					) : (
-						<option value={c.name}>{c.name}</option> || (
-							<option selected value="loading">
-								Loading...
-							</option>
-						)
-					)
-				)}
-			</select>
-			<br />
-			<hr />
-			<label htmlFor="level">CHARACTER LEVEL || </label>
-			<input
-				type="number"
-				name="level"
-				id="level"
-				value={level}
-				max={10}
-				min={1}
-				onChange={(e) => {
-					setLevel(parseInt(e.target.value));
-				}}
-			/>
-			<br />
-			<hr />
-			<label htmlFor="str">STRENGTH INPUT || </label>
-			<input
-				type="number"
-				name="str"
-				id="str"
-				value={strength}
-				max={20}
-				min={1}
-				onChange={(e) => {
-					setStrength(parseInt(e.target.value));
-				}}
-			/>
-			<br />
-			<hr />
-			<label htmlFor="dex">DEXTERITY INPUT || </label>
-			<input
-				type="number"
-				name="dex"
-				id="dex"
-				value={dexterity}
-				max={20}
-				min={1}
-				onChange={(e) => {
-					setDexterity(parseInt(e.target.value));
-				}}
-			/>
-			<br />
-			<hr />
-			<label htmlFor="con">CONSTITUTION INPUT || </label>
-			<input
-				type="number"
-				name="con"
-				id="con"
-				value={constitution}
-				max={20}
-				min={1}
-				onChange={(e) => {
-					setConstitution(parseInt(e.target.value));
-				}}
-			/>
-			<br />
-			<hr />
-			<label htmlFor="int">STRENGTH INPUT || </label>
-			<input
-				type="number"
-				name="int"
-				id="int"
-				value={intelligence}
-				max={20}
-				min={1}
-				onChange={(e) => {
-					setIntelligence(parseInt(e.target.value));
-				}}
-			/>
-			<br />
-			<hr />
-			<label htmlFor="wis">STRENGTH INPUT || </label>
-			<input
-				type="number"
-				name="wis"
-				id="wis"
-				value={wisdom}
-				max={20}
-				min={1}
-				onChange={(e) => {
-					setWisdom(parseInt(e.target.value));
-				}}
-			/>
-			<br />
-			<hr />
-			<label htmlFor="cha">STRENGTH INPUT || </label>
-			<input
-				type="number"
-				name="cha"
-				id="cha"
-				value={charisma}
-				max={20}
-				min={1}
-				onChange={(e) => {
-					setCharisma(parseInt(e.target.value));
-				}}
-			/>
-		</>
-	);
+  const classNameQuery = trpc.classNames.useQuery();
+  const [_name, setName] = useAtom(nameAtom);
+  const [_playerName, setPlayerName] = useAtom(playerAtom);
+  const [class_, setClass] = useAtom(classAtom);
+  const [level, setLevel] = useAtom(levelAtom);
+  const [strength, setStrength] = useAtom(strengthAtom);
+  const [dexterity, setDexterity] = useAtom(dexterityAtom);
+  const [constitution, setConstitution] = useAtom(constitutionAtom);
+  const [intelligence, setIntelligence] = useAtom(intelligenceAtom);
+  const [wisdom, setWisdom] = useAtom(wisdomAtom);
+  const [charisma, setCharisma] = useAtom(charismaAtom);
+  return (
+    <div className="container m-2">
+      <label htmlFor="name">CHARACTER NAME INPUT || </label>
+      <input
+        type="text"
+        name="name"
+        id="name"
+        onChange={(e) => {
+          setName(e.target.value);
+        }}
+      />
+      <br />
+      <hr />
+      <label htmlFor="playerName">PLAYER NAME INPUT || </label>
+      <input
+        type="text"
+        name="playerName"
+        id="playerName"
+        onChange={(e) => {
+          setPlayerName(e.target.value);
+        }}
+      />
+      <br />
+      <hr />
+      <label htmlFor="class">CLASS || </label>
+      <select
+        name="class"
+        onChange={(e) => {
+          setClass(e.target.value);
+        }}>
+        {classNameQuery.data?.map((c) =>
+          c.name === class_ ? (
+            <option selected value={c.name}>
+              {c.name}
+            </option>
+          ) : (
+            <option value={c.name}>{c.name}</option> || (
+              <option selected value="loading">
+                Loading...
+              </option>
+            )
+          )
+        )}
+      </select>
+      <br />
+      <hr />
+      <label htmlFor="level">CHARACTER LEVEL || </label>
+      <input
+        type="number"
+        name="level"
+        id="level"
+        value={level}
+        max={10}
+        min={1}
+        onChange={(e) => {
+          setLevel(parseInt(e.target.value));
+        }}
+      />
+      <br />
+      <hr />
+      <label htmlFor="str">STRENGTH INPUT || </label>
+      <input
+        type="number"
+        name="str"
+        id="str"
+        value={strength}
+        max={20}
+        min={1}
+        onChange={(e) => {
+          setStrength(parseInt(e.target.value));
+        }}
+      />
+      <br />
+      <hr />
+      <label htmlFor="dex">DEXTERITY INPUT || </label>
+      <input
+        type="number"
+        name="dex"
+        id="dex"
+        value={dexterity}
+        max={20}
+        min={1}
+        onChange={(e) => {
+          setDexterity(parseInt(e.target.value));
+        }}
+      />
+      <br />
+      <hr />
+      <label htmlFor="con">CONSTITUTION INPUT || </label>
+      <input
+        type="number"
+        name="con"
+        id="con"
+        value={constitution}
+        max={20}
+        min={1}
+        onChange={(e) => {
+          setConstitution(parseInt(e.target.value));
+        }}
+      />
+      <br />
+      <hr />
+      <label htmlFor="int">STRENGTH INPUT || </label>
+      <input
+        type="number"
+        name="int"
+        id="int"
+        value={intelligence}
+        max={20}
+        min={1}
+        onChange={(e) => {
+          setIntelligence(parseInt(e.target.value));
+        }}
+      />
+      <br />
+      <hr />
+      <label htmlFor="wis">STRENGTH INPUT || </label>
+      <input
+        type="number"
+        name="wis"
+        id="wis"
+        value={wisdom}
+        max={20}
+        min={1}
+        onChange={(e) => {
+          setWisdom(parseInt(e.target.value));
+        }}
+      />
+      <br />
+      <hr />
+      <label htmlFor="cha">STRENGTH INPUT || </label>
+      <input
+        type="number"
+        name="cha"
+        id="cha"
+        value={charisma}
+        max={20}
+        min={1}
+        onChange={(e) => {
+          setCharisma(parseInt(e.target.value));
+        }}
+      />
+    </div>
+  );
 }
