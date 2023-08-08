a03b79c883bae4abd3ec9701f123bee31575aaa1
thebadams <bpeteradams@gmail.com>
2023-08-07 20:17:09 -0400

    Tailwind: set up tailwind and prettier extensions in project

--- packages/character-builder-react/src/components/CharacterDisplay.tsx
+++ packages/character-builder-react/src/components/CharacterDisplay.tsx
@@ -2,38 +2,40 @@ import { useAtom } from 'jotai';
 import { currentCharacterAtom } from '../state/currentCharacter';
 
 export default function CharacterDisplay() {
-	const [currentCharacter] = useAtom(currentCharacterAtom);
-	return (
-		<>
-			<h2>Current Character</h2>
-			<h3>CHARACTER NAME || {currentCharacter.name}</h3>
-			<h3>PLAYER NAME || {currentCharacter.player}</h3>
-			<h3>CHARACTER CLASS || {currentCharacter.class}</h3>
-			<h3>CHARACTER LEVEL || {currentCharacter.level}</h3>
-			<h3>
-				STRENGTH || {currentCharacter.abilities.strength.score} ||{' '}
-				{currentCharacter.abilities.strength.mod}
-			</h3>
-			<h3>
-				DEXTERITY || {currentCharacter.abilities.dexterity.score} ||{' '}
-				{currentCharacter.abilities.dexterity.mod}
-			</h3>
-			<h3>
-				CONSTITUTION || {currentCharacter.abilities.constitution.score} ||{' '}
-				{currentCharacter.abilities.constitution.mod}
-			</h3>
-			<h3>
-				INTELLIGENCE || {currentCharacter.abilities.intelligence.score} ||{' '}
-				{currentCharacter.abilities.intelligence.mod}
-			</h3>
-			<h3>
-				WISDOM || {currentCharacter.abilities.wisdom.score} ||{' '}
-				{currentCharacter.abilities.wisdom.mod}
-			</h3>
-			<h3>
-				CHARISMA || {currentCharacter.abilities.charisma.score} ||{' '}
-				{currentCharacter.abilities.charisma.mod}
-			</h3>
-		</>
-	);
+  const [currentCharacter] = useAtom(currentCharacterAtom);
+  return (
+    <>
+      <h2 className=" text-center text-3xl text-red-700 shadow-sm shadow-black">
+        Current Character
+      </h2>
+      <h3>CHARACTER NAME || {currentCharacter.name}</h3>
+      <h3>PLAYER NAME || {currentCharacter.player}</h3>
+      <h3>CHARACTER CLASS || {currentCharacter.class}</h3>
+      <h3>CHARACTER LEVEL || {currentCharacter.level}</h3>
+      <h3>
+        STRENGTH || {currentCharacter.abilities.strength.score} ||{' '}
+        {currentCharacter.abilities.strength.mod}
+      </h3>
+      <h3>
+        DEXTERITY || {currentCharacter.abilities.dexterity.score} ||{' '}
+        {currentCharacter.abilities.dexterity.mod}
+      </h3>
+      <h3>
+        CONSTITUTION || {currentCharacter.abilities.constitution.score} ||{' '}
+        {currentCharacter.abilities.constitution.mod}
+      </h3>
+      <h3>
+        INTELLIGENCE || {currentCharacter.abilities.intelligence.score} ||{' '}
+        {currentCharacter.abilities.intelligence.mod}
+      </h3>
+      <h3>
+        WISDOM || {currentCharacter.abilities.wisdom.score} ||{' '}
+        {currentCharacter.abilities.wisdom.mod}
+      </h3>
+      <h3>
+        CHARISMA || {currentCharacter.abilities.charisma.score} ||{' '}
+        {currentCharacter.abilities.charisma.mod}
+      </h3>
+    </>
+  );
 }
